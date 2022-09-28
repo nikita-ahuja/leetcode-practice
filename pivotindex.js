0ae2ff72dad9ebed9635e52cb853988bd0662d41
Nikita Ahuja <nahuja038@gmail.com>
2022-09-27 22:26:26 -0500

    rewritten with descriptive variables

--- pivotindex.js
+++ pivotindex.js
@@ -2,51 +2,24 @@
  * @param {number[]} nums
  * @return {number}
  */
-var pivotIndex = function(nums) {
+ var pivotIndex = function(nums) {
 
-    for (let i = 0; i < nums.length; i++) {
+   for (let potentialPivot = 0; potentialPivot < nums.length; potentialPivot++) {
 
-        let sumLeft = 0;
-        for (let j = i - 1; j > -1; j--) {
-            sumLeft += nums[j]
-        }
+     let sumOfLeftHalf = 0;
+     for (let leftHalfIdx = potentialPivot - 1; leftHalfIdx > -1; leftHalfIdx--) {
+       sumOfLeftHalf += nums[leftHalfIdx]
+     }
 
-        let sumRight = 0;
-        for (let k = i + 1; k < nums.length; k++) {
-            sumRight += nums[k]
-        }
+     let sumOfRightHalf = 0;
+     for (let rightHalfIdx = potentialPivot + 1; rightHalfIdx < nums.length; rightHalfIdx++) {
+       sumOfRightHalf += nums[rightHalfIdx]
+     }
 
-        if (sumLeft === sumRight) {
-            return i
-        }
-    }
+     if (sumOfRightHalf == sumOfLeftHalf) {
+       return potentialPivot
+     }
 
-    return - 1
-};
-
-
-var pivotIndex = function(nums) {
-
-  for (let potentialPivot = 0; potentialPivot < nums.length; potentialPivot++) {
-
-    //left side
-    let sumOfLeftHalf = 0; //sum the left half of the array, starting at the index and traversing left
-    for (let leftHalfIdx = potentialPivot-1; leftHalfIdx >= -1; leftHalfIdx--) {
-      sumOfLeftHalf += nums[leftHalfIdx]
-    }
-
-    //right side
-    let sumOfRightHalf = 0;
-    for (let rightHalfIdx = potentialPivot+1; rightHalfIdx <= nums.length; rightHalfIdx++) {
-      sumofRightHalf += nums[rightHalfIdx]
-    }
-
-    if (sumOfRightHalf == sumOfLeftHalf) {
-      return potentialPivot;
-    }
-
-  }
-
-  return false
-
-};
+   }
+   return -1
+ };
