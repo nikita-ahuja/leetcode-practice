2d866f8245febfb465e97d7c99069cf663f1fe26
Nikita Ahuja <nahuja038@gmail.com>
2022-10-02 23:36:22 -0500

    wrote optimimzed solution

--- isomorphicstrings.js
+++ isomorphicstrings.js
@@ -37,10 +37,49 @@
 
 console.log(isIsomorphic("egg", "add"));
 
+/**  iteration 1
+ * 
+e = sCharacter
+a = tCharacter
+
+sMap {
+   e : a
+ }
+
+
+tMap {
+   a : e
+}
+
+*/ 
+
+
+
+
+
 
 var isIsomorphic = function(s, t) {
   //implement checks
   if (s.length !== t.length) {
     return false
   }
-}
+
+  let sArray = new Array(128).fill(0);
+  let tArray = new Array(128).fill(0);
+
+  for(let index =0; index <s.length; index++){
+    let sAscii = s.charCodeAt(index); //look at the string and get the ascii code of the lettr
+    let tAscii = t.charCodeAt(index);
+
+    if (sArray[sAscii] !== tArray[tAscii]) {
+      return false;
+    }
+
+    //denote that this is the position of the letter in the word (if count starting from 1)
+    sArray[sAscii]  = index + 1
+    tArray[tAscii] = index + 1
+  }
+
+  return true;
+};
+
